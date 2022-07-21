76dd66aabfa3e24b38ee01170a3f0b48cd338a83
williamtocto <william.tocto.est@tecazuay.edu.ec>
2022-07-21 00:44:27 -0500

    Base del proyecto

---
+++ src/main/java/com/example/ejerciciospracticos/EjerciciosPracticosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejerciciospracticos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciosPracticosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjerciciosPracticosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ejerciciospracticos/EjerciciosPracticosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejerciciospracticos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciosPracticosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
